3f70d30e19fe1bb1bd2608d02cb2d8a5043f7985
laughing-nerd <riturajpaul99@gmail.com>
2023-07-09 22:51:34 +0530

    Rituraj/Login_done

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -6,26 +6,27 @@ export default function Home() {
     <div className={styles.main}>
       <div className={styles.top}>
         <div className={styles.left_div}>
-          <svg viewBox="0 0 1000 1080" fill="none">
-            <g filter="url(#filter0_d_93_102)">
-              <path d="M947 818C781.274 705.842 352.232 966.5 72.5 966.5C-207.232 966.5 -434 739.732 -434 460C-434 180.268 -207.232 -46.5 72.5 -46.5C352.232 -46.5 1194.5 985.5 947 818Z" fill="url(#paint0_linear_93_102)" />
-            </g>
-            <defs>
-              <filter id="filter0_d_93_102" x="-438" y="-46.5" width="1433.96" height="1021" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
-                <feFlood flood-opacity="0" result="BackgroundImageFix" />
-                <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha" />
-                <feOffset dy="4" />
-                <feGaussianBlur stdDeviation="2" />
-                <feColorMatrix type="matrix" values="0 0 0 0 0.647059 0 0 0 0 0.643137 0 0 0 0 0.643137 0 0 0 1 0" />
-                <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_93_102" />
-                <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_93_102" result="shape" />
-              </filter>
-              <linearGradient id="paint0_linear_93_102" x1="-114.5" y1="61.9999" x2="385.5" y2="910" gradientUnits="userSpaceOnUse">
-                <stop offset="0.221679" stop-color="#B32346" />
-                <stop offset="0.66922" stop-color="#6A0B37" />
-              </linearGradient>
-            </defs>
-          </svg>
+        <svg viewBox="0 0 1000 1080" fill="none">
+  <g filter="url(#filter0_d_93_102)">
+    <path d="M947 818C781.274 705.842 352.232 966.5 72.5 966.5C-207.232 966.5 -434 739.732 -434 460C-434 180.268 -207.232 -46.5 72.5 -46.5C352.232 -46.5 1194.5 985.5 947 818Z" fill="url(#paint0_linear_93_102)" />
+  </g>
+  <defs>
+    <filter id="filter0_d_93_102" x="-438" y="-46.5" width="1433.96" height="1021" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+      <feFlood flood-opacity="0" result="BackgroundImageFix" />
+      <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha" />
+      <feOffset dy="4" />
+      <feGaussianBlur stdDeviation="2" />
+      <feColorMatrix type="matrix" values="0 0 0 0 0.647059 0 0 0 0 0.643137 0 0 0 0 0.643137 0 0 0 1 0" />
+      <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_93_102" />
+      <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_93_102" result="shape" />
+    </filter>
+    <linearGradient id="paint0_linear_93_102" x1="-114.5" y1="61.9999" x2="385.5" y2="910" gradientUnits="userSpaceOnUse">
+      <stop offset="0.221679" stop-color="darkred" />
+      <stop offset="0.66922" stop-color="maroon" />
+    </linearGradient>
+  </defs>
+</svg>
+
         </div>
 
         <div className={styles.right_div}>
